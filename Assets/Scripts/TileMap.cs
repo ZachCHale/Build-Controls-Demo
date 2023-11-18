6cd038cfbac75259581bd6dc2e8e21541d8ece2b
ZachCHale <ZH_Dev@pm.me>
2023-11-18 13:09:01 -0500

    Added and changed named variables for better readability, Added out parameters for better edgecase handling.

--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -17,73 +17,78 @@ public class TileMap : MonoBehaviour
         {
             _tileMap = tileMap;
             MapIndex = mapIndex;
-            WorldPosition = tileMap._mapOrigin + new Vector3(_tileMap._gapDistance / 2, 0, _tileMap._gapDistance / 2) +
-                            new Vector3(tileMap._gapDistance * MapIndex.x, 0, tileMap._gapDistance * mapIndex.y);
+            float tileOffset = _tileMap._tileSize / 2;
+            Vector3 mapCornerOffsetPosition = _tileMap._mapOriginWorld + new Vector3(tileOffset, 0, tileOffset);
+            WorldPosition = mapCornerOffsetPosition + new Vector3(tileMap._tileSize * MapIndex.x, 0, tileMap._tileSize * mapIndex.y);
         }
 
-        public PlaceableObjData PlaceNewObject(PlaceableObjData data)
+        public bool PlaceNewObject(PlaceableObjData objData, out PlaceableObjData removedObjData)
         {
-            PlaceableObjData prevObjectData = _placedObject?.Data;
+            bool tileWasEmpty = _placedObject == null;
+            removedObjData  = _placedObject?.Data;
             _placedObject?.Dispose();
-            _placedObject = new PlaceableObjInstance(data, WorldPosition);
-            return prevObjectData;
+            _placedObject = new PlaceableObjInstance(objData, WorldPosition);
+            return tileWasEmpty;
         }
-        
-        
     }
     
-    private readonly int _lengthX = 10;
-    private readonly int _lengthY = 20;
-    private readonly float _gapDistance = 1f;
+    private readonly int _tileCountX = 10;
+    private readonly int _tileCountY = 20;
+    private readonly float _tileSize = 1f;
     private Camera _mainCamera;
-
-
+    
     private readonly Dictionary<Vector2, Tile>_tiles = new ();
-    private Vector3 _mapOrigin => transform.position;
+    private Vector3 _mapOriginWorld => transform.position;
+    private Vector3 _mapEndWorld => transform.position + _tileSize * ((_mapXDir * _tileCountX) + (_mapYDir * _tileCountY));
+    private Vector3 _mapOriginLocal => Vector3.zero;
+    private Vector3 _mapEndLocal => new Vector3(_tileSize * _tileCountX, 0, _tileSize * _tileCountY);
+
     private Vector3 _mapNormal => transform.up;
     private Vector3 _mapXDir => transform.right;
     private Vector3 _mapYDir => transform.forward;
-
     
     private void Awake()
     {
         _mainCamera = Camera.main;
-        foreach (int x in Enumerable.Range(0, _lengthX-1))
-            foreach (int y in Enumerable.Range(0, _lengthY-1))
+        foreach (int x in Enumerable.Range(0, _tileCountX))
+            foreach (int y in Enumerable.Range(0, _tileCountY))
                 _tiles.Add(new(x,y), new Tile(this, new(x,y)));
     }
 
-    public Vector3 WorldSpaceToMapSpace(Vector3 worldPos)
+    public Vector3 WorldSpaceToLocalMapSpace(Vector3 worldPos)
     {
-        Vector3 xProjection = Vector3.Project(worldPos - _mapOrigin, transform.right);
-        Vector3 yProjection = Vector3.Project(worldPos - _mapOrigin,transform.forward);
+        Vector3 xProjection = Vector3.Project(worldPos - _mapOriginWorld, _mapXDir);
+        Vector3 yProjection = Vector3.Project(worldPos - _mapOriginWorld,_mapYDir);
 
         float localX = xProjection.magnitude * Math.Sign(Vector3.Dot(xProjection, _mapXDir));
         float localY = yProjection.magnitude * Math.Sign(Vector3.Dot(yProjection, _mapYDir));
         
         return new(localX, 0, localY);
     }
-
-    public Vector2 WorldSpaceToMapIndex(Vector3 worldPos)
+    
+    public bool WorldSpaceToMapTileIndex(Vector3 worldPos, out Vector2 mapIndex)
     {
-        Vector3 localPos = WorldSpaceToMapSpace(worldPos);
-        if (localPos.x < 0 || localPos.x > _lengthX * _gapDistance || localPos.z < 0 ||
-            localPos.z > _lengthY * _gapDistance)
-            return new (-1, -1);
-        return new((int)(localPos.x / _gapDistance), (int)(localPos.z / _gapDistance));
+        Vector3 localPos = WorldSpaceToLocalMapSpace(worldPos);
+        bool withinBounds = localPos.x >= _mapOriginLocal.x && 
+                             localPos.x <= _mapEndLocal.x && 
+                             localPos.z >= _mapOriginLocal.z &&
+                             localPos.z <=_mapEndLocal.z;
+        int xIndex = (int)Math.Clamp(localPos.x / _tileSize, _mapOriginLocal.x, _mapEndLocal.x);
+        int yIndex = (int)Math.Clamp(localPos.z / _tileSize, _mapOriginLocal.z, _mapEndLocal.z);
+        
+        mapIndex = new(xIndex, yIndex);
+        return withinBounds;
     }
 
     public bool PlaceObjectAtMousePosition(PlaceableObjData objData)
     {
-        Vector3 worldPos = _mainCamera.GetScreenPointIntersectionWithPlane(Input.mousePosition, _mapNormal, _mapOrigin);
-        Vector2 mapIndex = WorldSpaceToMapIndex(worldPos);
+        bool validPosition = _mainCamera.GetScreenPointIntersectionWithPlane(Input.mousePosition, _mapNormal, _mapOriginWorld, out Vector3 worldPos);
+        bool inBounds = WorldSpaceToMapTileIndex(worldPos, out Vector2 mapIndex);
         
-        if (mapIndex.Equals(new(-1, -1))) return false;
-
-        Assert.IsTrue(_tiles.ContainsKey(mapIndex));
+        if (!inBounds || !validPosition) return false;
         
-        PlaceableObjData oldObjData = _tiles[mapIndex].PlaceNewObject(objData);
-        if(oldObjData != null)Debug.Log($"Removed:{oldObjData.Label}");
+        bool objectWasRemoved = !_tiles[mapIndex].PlaceNewObject(objData, out PlaceableObjData removedObjData);
+        if(objectWasRemoved)Debug.Log($"Removed:{removedObjData.Label}");
         return true;
     }
 }
