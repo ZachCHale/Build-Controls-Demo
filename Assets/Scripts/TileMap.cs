1722ebbdb1b1a89bebe713d23583d6acb1f80b99
ZachCHale <ZH_Dev@pm.me>
2023-11-18 14:29:41 -0500

    Documented TIleMap

--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Assertions;
 
 public class TileMap : MonoBehaviour
 {
@@ -12,16 +11,27 @@ public class TileMap : MonoBehaviour
         public readonly Vector3 WorldPosition;
         public readonly Vector2 MapIndex;
         private PlaceableObjInstance _placedObject;
-        
+        /// <summary>
+        /// <para>Creates a tile and initializes its world position based on the given <c>TileMap</c> and tile index.</para>
+        /// </summary>
+        /// <param name="tileMap">The <c>TileMap</c> containing this tile.</param>
+        /// <param name="mapIndex">The index/key for this tile in the <c>TileMap</c></param>
         public Tile(TileMap tileMap, Vector2 mapIndex)
         {
             _tileMap = tileMap;
             MapIndex = mapIndex;
-            float tileOffset = _tileMap._tileSize / 2;
-            Vector3 mapCornerOffsetPosition = _tileMap._mapOriginWorld + new Vector3(tileOffset, 0, tileOffset);
-            WorldPosition = mapCornerOffsetPosition + new Vector3(tileMap._tileSize * MapIndex.x, 0, tileMap._tileSize * mapIndex.y);
+            float tileCenterOffset = _tileMap._tileSize / 2;
+            Vector3 mapCornerCenterOffsetPosition = _tileMap._mapOriginWorld + new Vector3(tileCenterOffset, 0, tileCenterOffset);
+            WorldPosition = mapCornerCenterOffsetPosition + new Vector3(tileMap._tileSize * MapIndex.x, 0, tileMap._tileSize * mapIndex.y);
         }
-
+        /// <summary>
+        /// <para>For a given <c>PlaceableObjData</c>, dispose of any object instance in the tile and create a new
+        /// <c>PlaceableObjectInstance</c> at the center of the tile.</para>
+        /// </summary>
+        /// <param name="objData"><c>PlaceableObjData</c> to create a <c>PlaceableObjectInstance</c> from.</param>
+        /// <param name="removedObjData">When this method returns, contains the <c>PlaceableObjData</c> of the <c>PlaceableObjectInstance</c>
+        /// that was removed from the tile. If no tile was removed, remains null.</param>
+        /// <returns><c>true</c> if the tile was empty. <c>false</c> if an object was removed to add this one.</returns>
         public bool PlaceNewObject(PlaceableObjData objData, out PlaceableObjData removedObjData)
         {
             bool tileWasEmpty = _placedObject == null;
@@ -54,7 +64,11 @@ public class TileMap : MonoBehaviour
             foreach (int y in Enumerable.Range(0, _tileCountY))
                 _tiles.Add(new(x,y), new Tile(this, new(x,y)));
     }
-
+    /// <summary>
+    /// <para>For a given position in world space, returns the position relative to the map's position and orientation.</para>
+    /// </summary>
+    /// <param name="worldPos">Position to convert to a local map position.</param>
+    /// <returns>New position relative to the map's position and orientation.</returns>
     public Vector3 WorldSpaceToLocalMapSpace(Vector3 worldPos)
     {
         Vector3 xProjection = Vector3.Project(worldPos - _mapOriginWorld, _mapXDir);
@@ -65,7 +79,13 @@ public class TileMap : MonoBehaviour
         
         return new(localX, 0, localY);
     }
-    
+    /// <summary>
+    /// <para>For a given position in world space, finds the index of the tile containing that point. </para>
+    /// </summary>
+    /// <param name="worldPos">The position to find the tile index of.</param>
+    /// <param name="mapIndex">When this method returns, contains the index for the tile containing the point if the point
+    /// is within the bounds of the map; otherwise, contains the index of the closest edge tile.</param>
+    /// <returns><c>true</c> if the position is within the bounds of the map. <c>false</c> if the position is out of bounds.</returns>
     public bool WorldSpaceToMapTileIndex(Vector3 worldPos, out Vector2 mapIndex)
     {
         Vector3 localPos = WorldSpaceToLocalMapSpace(worldPos);
@@ -79,7 +99,12 @@ public class TileMap : MonoBehaviour
         mapIndex = new(xIndex, yIndex);
         return withinBounds;
     }
-
+    /// <summary>
+    /// For a given <c>PlaceableObjData</c>, Creates a <c>PlaceableObjInstance</c> and adds it to the tile under the mouse.
+    /// </summary>
+    /// <param name="objData"><c>PlaceableObjData</c> to create a <c>PlaceableObjInstance</c> from.</param>
+    /// <returns><c>true</c> if the object was placed successfully. <c>false</c> if the mouse is pointing out of the bounds,
+    /// in the opposite direction of the map, or parallel to the map. </returns>
     public bool PlaceObjectAtMousePosition(PlaceableObjData objData)
     {
         bool validPosition = _mainCamera.GetScreenPointIntersectionWithPlane(Input.mousePosition, _mapNormal, _mapOriginWorld, out Vector3 worldPos);
